7adac7fd23127fc1e6b6b4bd70c331d98ceecc5a
krisshsaahi49 <krisshsaahi@outlook.com>
2024-11-26 16:53:28 -0500

    updated TLS settings in cloudfare to disable captch -> This change needs to  revert back

--- install_driver.sh
+++ install_driver.sh
@@ -1,7 +1,7 @@
 sh
 # Download and Install chromedriver
-wget -N https://storage.googleapis.com/chrome-for-testing-public/131.0.6778.85/linux64/chromedriver-linux64.zip -P ~/
-unzip ~/chromedriver-linux64.zip -d ~/
+#wget -N https://storage.googleapis.com/chrome-for-testing-public/131.0.6778.85/linux64/chromedriver-linux64.zip -P ~/
+#unzip ~/chromedriver-linux64.zip -d ~/
 
 # Install chrome browser
 curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
--- tests/test_login.py
+++ tests/test_login.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from pages.loginpage import LoginPage
+from selenium_stealth import stealth
 
 URL = "https://automation.krisshsaahi.dev"
 
@@ -10,12 +11,24 @@ URL = "https://automation.krisshsaahi.dev"
 def driver():
     option = webdriver.ChromeOptions()
     option.add_argument("headless")
+    option.add_argument("--start-maximized")
     option.add_argument("--no-sandbox")
     option.add_argument("--disable-extensions")
     option.add_argument("--disable-gpu")
-    service = ChromeService(executable_path="/root/chromedriver-linux64/chromedriver")
-    driver = webdriver.Chrome(service=service, options=option)
-    # driver = webdriver.Chrome(service=ChromeService(executable_path=ChromeDriverManager().install()), options=option)
+    option.add_experimental_option("excludeSwitches", ["enable-automation"])
+    option.add_experimental_option('useAutomationExtension', False)
+    # service = ChromeService(executable_path="/root/chromedriver-linux64/chromedriver")
+    # driver = webdriver.Chrome(service=service, options=option)
+    driver = webdriver.Chrome(service=ChromeService(executable_path=ChromeDriverManager().install()), options=option)
+    stealth(driver,
+            languages=["en-US", "en"],
+            user_agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
+            vendor="Google Inc.",
+            platform="Win32",
+            webgl_vendor="Intel Inc.",
+            renderer="Intel Iris OpenGL Engine",
+            fix_hairline=True,
+            )
     driver.delete_all_cookies()
     driver.maximize_window()
     driver.implicitly_wait(15)
